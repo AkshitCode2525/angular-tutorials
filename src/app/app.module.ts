c97c2e4800a3d5717f5f68db06d24823881130e2
AkshitCode2525 <akshitcode2525@gmail.com>
2022-08-17 17:22:52 +0530

    2 files modified, 13 added, 13 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,31 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { RootComponent } from './root/root.component';
-import { ProductListComponent } from './root/product-list/product-list.component';
-import { ProductAlertComponent } from './root/product-alert/product-alert.component';
-import { TopBarComponent } from './root/top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertComponent } from './product-alert/product-alert.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { RouterModule } from '@angular/router';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+    ]),
+  ],
   declarations: [
     AppComponent,
     HelloComponent,
-    RootComponent,
     ProductListComponent,
     ProductAlertComponent,
     TopBarComponent,
+    ProductDetailsComponent,
   ],
   bootstrap: [AppComponent],
 })
