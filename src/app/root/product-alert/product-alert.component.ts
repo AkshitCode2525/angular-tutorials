0cb161b38a9a781bfa5cf58e5c258834a0570adb
AkshitCode2525 <akshitcode2525@gmail.com>
2022-08-17 14:35:16 +0530

    2 files modified, 1 added, 1 deleted

--- src/app/root/product-alert/product-alert.component.ts
+++ src/app/root/product-alert/product-alert.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { products } from '../products';
 @Component({
   selector: 'app-product-alert',
   templateUrl: './product-alert.component.html',
-  styleUrls: ['./product-alert.component.css']
+  styleUrls: ['./product-alert.component.css'],
 })
 export class ProductAlertComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
